164100be9715cd57065139947f50919d890c8b6c
teresa <uo288347@uniovi.com>
2025-10-15 13:13:38 +0200

    Arreglar problemillas

--- script.js
+++ script.js
@@ -20,14 +20,16 @@ function renderScreen() {
   }
 }
 
+function showData(data) {
+    //console.log(data); 
+    $output.html(`<pre>${JSON.stringify(data, null, 2)}</pre>`);
+  }
+
 function attachButtonEvents($btn, $output) {
   let downTime = null, upTime = null, moves = [];
   $btn.off(); // Limpia eventos previos
 
-  function showData(data) {
-    //console.log(data); 
-    $output.html(`<pre>${JSON.stringify(data, null, 2)}</pre>`);
-  }
+  
 
   $btn.on('pointerdown', function(e) {
     downTime = Date.now();
