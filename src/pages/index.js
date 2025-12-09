80561d009fce309e87c9cd76cce7b0fa40dbbd36
teresa <uo288347@uniovi.com>
2025-12-09 13:03:58 +0100

    language index

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,9 @@ export default function Index() {
     <div style={{flex: 1, padding: "20px 20px", 
       display: "flex", flexDirection: "column", justifyContent: "center", 
       alignItems: "center", position:"relative", overflow: "hidden"}}>
-    
+    <div style={{ position: "absolute", top: "20px", right: "20px" }}>
+      <LanguageSwitcher />
+    </div>
     <Button style={{width:"100%"}} size="large" type="primary" onClick={handleStart}>
       {t('auth.start')}
     </Button>
