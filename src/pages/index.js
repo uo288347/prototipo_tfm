3b3bac77bf0bc511b5e8d5180ad52d076fff0722
teresa <uo288347@uniovi.com>
2025-12-09 13:05:17 +0100

    change

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,14 +12,17 @@ export default function Index() {
 
   return (
     <div style={{flex: 1, padding: "20px 20px", 
-      display: "flex", flexDirection: "column", justifyContent: "center", 
+      display: "flex", flexDirection: "column", justifyContent: "space-between", 
       alignItems: "center", position:"relative", overflow: "hidden"}}>
     <div style={{ position: "absolute", top: "20px", right: "20px" }}>
       <LanguageSwitcher />
     </div>
-    <Button style={{width:"100%"}} size="large" type="primary" onClick={handleStart}>
+    <div style={{flex: 1, display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+<Button style={{width:"100%"}} size="large" type="primary" onClick={handleStart}>
       {t('auth.start')}
     </Button>
+    </div>
+    
     </div>
   );
 }
