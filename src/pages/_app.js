f12371ee44fdf7ff2930de5ab343b4312172b353
teresa <uo288347@uniovi.com>
2026-02-05 10:13:07 +0100

    prueba con dynamic viewport height

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -117,7 +117,7 @@ export default function App({ Component, pageProps }) {
                     <AntdMobileConfigProvider locale={antdMobileLocale}>
                         {contextHolder}
                         <div style={{
-                            minHeight: "100vh",
+                            minHeight: "100dvh",
                             display: "flex",
                             flexDirection: "column",
                             backgroundColor: "#fff"
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -3,7 +3,7 @@ import { CheckoutComponent } from "@/components/CheckoutComponent";
 export default function Checkout({footer}) {
 
     return (
-        <div style={{ flex:1, display:"flex", flexDirection:"column", height: "100%", padding: "0px 20px"}}>
+        <div style={{ flex:1, display:"flex", flexDirection:"column", height: "100%", padding: "0px 10px"}}>
             <CheckoutComponent />
             {footer}
         </div>
@@ -12,7 +12,6 @@ export default function Checkout({footer}) {
 
 export async function getServerSideProps(context) {
   const locale = context.locale || 'en';
-  
   return {
     props: {
       messages: (await import(`../../messages/${locale}.json`)).default,
