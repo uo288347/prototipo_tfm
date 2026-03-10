8fc8be3f2ea64e98bf3b5ec6dc303ef648cd3640
teresa <uo288347@uniovi.com>
2026-03-10 10:30:54 +0100

    bug 5, metadatos document

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      </Head>
       <body>
         <Main />
         <NextScript />
